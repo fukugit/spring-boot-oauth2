f54aeb3d7f06ffafe7acbf23780001e4b76c7c16
fukugit <fukuma1234@gmail.com>
2018-09-16 11:03:00 +0900

    Add oauth2

--- src/main/java/jp/co/example/App.java
+++ src/main/java/jp/co/example/App.java
@@ -1,13 +1,67 @@
 package jp.co.example;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
+
 
 @SpringBootApplication
-public class App 
+@EnableOAuth2Sso
+@RestController
+public class App extends WebSecurityConfigurerAdapter 
 {
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class, args);
-    }
+  @Autowired
+  OAuth2ClientContext oauth2ClientContext;
+
+  @RequestMapping("/user")
+  public Principal user(Principal principal) {
+    return principal;
+  }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .antMatcher("/**")
+      .authorizeRequests()
+        .antMatchers("/", "/login**", "/webjars/**", "/error**")
+        .permitAll()
+      .anyRequest()
+        .authenticated();
+  }
+  
+  public static void main(String[] args) {
+      SpringApplication.run(App.class, args);
+  }
+
 }
+
